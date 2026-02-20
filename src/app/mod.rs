bad5ba8b4cc32b3cd12a527f64888d2999c5bcdc
oberprah <h.oberprantacher@gmail.com>
2026-02-20 12:34:22 +0100

    Add ToggleVisualMode action with v keybinding

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -88,6 +88,15 @@ impl App {
             Action::ToggleFolder => self.toggle_folder(false, false),
             Action::ExpandFolder => self.toggle_folder(true, false),
             Action::CollapseFolder => self.toggle_folder(false, true),
+            Action::ToggleVisualMode => {
+                if let View::Log(log) = &mut self.view {
+                    if log.is_visual_mode() {
+                        log.exit_visual_mode();
+                    } else {
+                        log.enter_visual_mode();
+                    }
+                }
+            }
             Action::Resize { .. } | Action::None => {}
         }
 
